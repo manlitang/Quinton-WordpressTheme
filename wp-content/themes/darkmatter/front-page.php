d239da74a0d73bae1ba2a4ea689d856ee7462c37
manlitang <44969715+manlitang@users.noreply.github.com>
2020-05-13 23:18:03 +0200

    feat(hero) make hero mobile friendly

--- wp-content/themes/darkmatter/front-page.php
+++ wp-content/themes/darkmatter/front-page.php
@@ -1,3 +1,21 @@
 <?php get_header();?>
 
+<section class="hero">
+    <video autoplay muted loop>
+        <source src="<?php 
+                $bg_vid = get_field('hero_video'); 
+                $url = $bg_vid['url'];
+                echo $url
+            ?>" type="video/mp4">
+    </video>
+    <div class="hero__text">
+        <h1><?php the_field('hero_headline'); ?></h1>
+
+        <div class="hero__cta">
+            <div class="play-btn"><?php the_field('hero_icon'); ?></div>
+            <p><?php the_field('hero_cta'); ?></p>
+        </div>
+    </div>
+</section>
+
 <?php get_footer();?>
