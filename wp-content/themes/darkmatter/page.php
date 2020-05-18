cb6f386f48bf52614523f9a4c32e215cef499895
manlitang <44969715+manlitang@users.noreply.github.com>
2020-05-18 18:52:25 +0200

    style(projects) add project content and adjust style accordingly

--- wp-content/themes/darkmatter/page.php
+++ wp-content/themes/darkmatter/page.php
@@ -34,6 +34,96 @@ if (is_page( 'projects' )) {
         <li>Corporate</li>
     </ul>
 </div>
+
+<section id="projects" class="section-centered section-centered--no-padding-bottom">
+    <?php 
+        $project1 = get_field('project1');
+        $project2 = get_field('project2');
+        $project3 = get_field('project3');
+        $project4 = get_field('project4');
+    ?>
+
+    <div class="projects-container">
+
+        <?php 
+            $project1 = get_field('project1');
+            $img1 = $project1['image'];
+            $url = $img1['url'];
+            $alt = $img1['alt'];
+
+            $logo_img1 = $project1['logo'];
+
+        ?>
+
+        <div class="project-image-container" data-tilt data-tilt-glare data-tilt-max-glare="0.2" data-tilt-perspective="8000" data-tilt-speed="100" data-tilt-reverse="true">
+            <a href="<?php echo $project1['link']; ?>">
+            <img src="<?php echo $img1['url'] ?>" alt="<?php $img1['alt'] ?>">
+                <div class="logo-overlay">
+                    <img src="<?php echo $logo_img1['url'] ?>" alt="<?php echo $logo_img1['alt'] ?>">
+                </div>
+            </a>
+        </div>
+
+        <?php 
+            $project2 = get_field('project2');
+            $img2 = $project2['image'];
+            $url = $img2['url'];
+            $alt = $img2['alt'];
+
+            $logo_img2 = $project2['logo'];
+
+        ?>
+
+        <div class="project-image-container" data-tilt data-tilt-glare data-tilt-max-glare="0.2" data-tilt-perspective="8000" data-tilt-speed="100" data-tilt-reverse="true">
+            <a href="<?php echo $project2['link']; ?>">
+            <img src="<?php echo $img2['url'] ?>" alt="<?php $img2['alt'] ?>">
+                <div class="logo-overlay">
+                    <img src="<?php echo $logo_img2['url'] ?>" alt="<?php echo $logo_img2['alt'] ?>">
+                </div>
+            </a>
+        </div>
+
+        <?php 
+            $project3 = get_field('project3');
+            $img3 = $project3['image'];
+            $url = $img3['url'];
+            $alt = $img3['alt'];
+
+            $logo_img3 = $project3['logo'];
+
+        ?>
+
+        <div class="project-image-container" data-tilt data-tilt-glare data-tilt-max-glare="0.2" data-tilt-perspective="8000" data-tilt-speed="100" data-tilt-reverse="true">
+            <a href="<?php echo $project3['link']; ?>">
+            <img src="<?php echo $img3['url'] ?>" alt="<?php $img3['alt'] ?>">
+                <div class="logo-overlay">
+                    <img src="<?php echo $logo_img3['url'] ?>" alt="<?php echo $logo_img3['alt'] ?>">
+                </div>
+            </a>
+        </div>
+
+        <?php 
+            $project4 = get_field('project4');
+            $img4 = $project4['image'];
+            $url = $img4['url'];
+            $alt = $img4['alt'];
+
+            $logo_img4 = $project4['logo'];
+
+        ?>
+
+        <div class="project-image-container" data-tilt data-tilt-glare data-tilt-max-glare="0.2" data-tilt-perspective="8000" data-tilt-speed="100" data-tilt-reverse="true">
+            <a href="<?php echo $project4['link']; ?>">
+            <img src="<?php echo $img4['url'] ?>" alt="<?php $img4['alt'] ?>">
+                <div class="logo-overlay">
+                    <img src="<?php echo $logo_img4['url'] ?>" alt="<?php echo $logo_img4['alt'] ?>">
+                </div>
+            </a>
+        </div>
+
+        <div class="bg-overlap"></div>
+    </div>
+</section>
 <?php }
 ?>
 
